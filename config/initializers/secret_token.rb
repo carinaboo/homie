7972612826aa46c25cc7fd1ca4267aff2e4978ca
Carina <carinaboo@berkeley.edu>
2014-02-10 19:06:16 -0800

    starter rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homie::Application.config.secret_key_base = '793f4569ee90d20a7290e2bd41e10fe2cf5c695166bcf839678c186b95e443e87c8f5da3d40598a35cd86965343eb930b2eaba03292cf65cb88caed8c64568b0'
