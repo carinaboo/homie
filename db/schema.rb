b9f0219ad27cb0c38710970c0075fca54be59733
Rico Ardisyah <rardisyah@gmail.cm>
2014-03-08 15:19:53 -0800

    Add username field for User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305210655) do
+ActiveRecord::Schema.define(version: 20140308230658) do
 
   create_table "apartments", force: true do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140305210655) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
