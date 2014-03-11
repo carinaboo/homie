bd21e3a11710a68caf2926b2d0b4abbbd3b37dc2
Yuhua Wang <yuhuaw@berkeley.edu>
2014-03-11 14:32:41 -0700

    Updated apartment controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -7,35 +7,64 @@ class ApartmentsController < ApplicationController
     @apartment = Apartment.find(params[:id])
     @reviews = Review.where(apartment_id: @apartment.id)
     #boolean value used in view to decide whether user can add new review
-    @show_form = can_review?(@reviews)        
+    @show_form = can_review?(@reviews)        #method from ApartmentsHelper
   end
 
   #Page for creating new apartment profile.
   def new
-    @apartment = Apartment.new
+    if current_user
+      @apartment = Apartment.new
+    else
+      render json: {errCode: 403}
+    end
   end
 
   #Creates and saves new apartment profile if information is valid; otherwise reloads page for
   #creating new profiles.
   def create
-    @apartment = Apartment.new(params[:apartment])
-    @apartment.save ? redirect_to @apartment : render "new"
+    user_id = current_user.id
+    title = params[:apartment][:title]
+    addr = params[:apartment][:address]
+    desc = params[:apartment][:desc]
+    price = params[:apartment][:price]
+    beds = params[:apartment][:bedrooms]
+    baths = params[:apartment][:bathrooms]
+    result = Apartment.add(user_id, title, addr, desc, price, beds, baths)
+    valid = result >= 0 && result != 403 && result != 404
+    if valid 
+      render json: {errCode: 1}
+    else
+      render json: {errCode: result}
+    end
   end
 
   #Page for editing apartment profile information.
   def edit
+    @apartment = Apartment.find(params[:id])
+    if !@apartment
+      render json: {errCode: 404}
+    elsif !current_user
+      render json: {errCode: 403}
+    end
   end
 
   #Saves updates to apartment profile if changes are valid; otherwise reload edit page.
   def update
     @apartment = Apartment.find(params[:id])
-    valid = @apartment.update_attributes(apartment_params)
-    valid ? redirect_to @apartment : render "edit"
-  end
-
-  #Verifies the user input contains the required fields of information. 
-  private
-    def apartment_params
-      params.require(:apartment).permit(:title, :address, :description, :price, :bathrooms, :bedrooms)
+    user_id = current_user.id
+    title = params[:apartment][:title]
+    addr = params[:apartment][:address]
+    desc = params[:apartment][:desc]
+    price = params[:apartment][:price]
+    beds = params[:apartment][:bedrooms]
+    baths = params[:apartment][:bathrooms]
+    result = Apartment.updateDescription(user_id, title, addr, desc, price, beds, baths)
+    valid = result >= 0 && result != 403 && result != 404
+    if valid 
+      render json: {errCode: 1}
+    else
+      render json: {errCode: result}
     end
+  end
+ 
 end
--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,4 @@
 class Apartment < ActiveRecord::Base
   has_many :reviews
+  belongs_to :user
 end
