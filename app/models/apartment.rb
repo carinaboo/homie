e5e423b30c2eba578d3859680a4c06bed02a2ed2
Lesley <chiang.lesley@gmail.com>
2014-03-11 22:03:00 -0700

    add and updateDescription function for apartment model

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,35 @@
 class Apartment < ActiveRecord::Base
-  has_many :reviews
-  belongs_to :user
+	has_many :reviews
+	belongs_to :user
+
+  	FORBIDDEN = 403
+
+	def self.add(user_id, title, address, desc, price, bedrooms, bathrooms)
+		addr = self.find_by_address(address)
+		if addr != nil || address = addr.address
+			#ERROR
+		elsif title == nil || address == nill || desc == nill || price == nill || bedrooms == nill || bathrooms == nill
+			#ERROR
+		elsif #user not logged in
+		else
+			apt = self.new(title: title, address: address, description: desc, price: price, bedrooms: bedrooms, bathrooms:bathrooms)
+			#SUCCESS
+	end
+
+	def self.updateDescription(user_id, title, address, desc, price, bederooms, bathrooms)
+		addr = self.find_by_address(address)
+		if addr == nil
+			#ERROR
+		elsif title == nil || address == nill || desc == nill || price == nill || bedrooms == nill || bathrooms == nill
+			#ERROR
+		elsif #user not logged in
+		else
+			apt = self.new(title: title, address: address, description: desc, price: price, bedrooms: bedrooms, bathrooms:bathrooms)
+			#SUCCESS
+	end
+
+	def self.search(address)
+
+	end
+
 end
