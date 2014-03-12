675188e9b19b400f1b74333400ddcfb17bf993fb
Jiahao Li <jiahaoli@berkeley.edu>
2014-03-11 22:02:35 -0700

    review controller and model v2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,16 @@ class ReviewsController < ApplicationController
   		#To do: render error code is user is not loggin in
   		render 
 
-  		Review.count(:conditions => "apt_id == params[:apt_id]")
+  	#update new averge rating
+	num_of_reviews = Review.count(:conditions => "apt_id == params[:apt_id]") 	#TODO: make sure this is correct
+	average_review = Apartment.find(params[:apt_id].average_overall_rating
+	total = num_of_reviews * average_review + params[:overall_rating]
+	num_of_reviews += 1
+	new_average = total / num_of_reviews
+	Apartment.average_overall_rating = new_average;
+	Apartment.save;
+
+	
 
   	#render json based on the error code returned
   end
