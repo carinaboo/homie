0c1b6bb84833022f17fdede09296f631f30a043d
Jiahao Li <jiahaoli@berkeley.edu>
2014-03-13 20:07:31 -0700

    review controller v7

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,6 @@ class ReviewsController < ApplicationController
   NOT_LOGGEDIN = -6
   UNAUTHORIZED_USER = -7
 
-
-
   def create
   	#assuming add method in model will return correct error code or new_average
   	if(current_user)	#check if the user is loggin in
@@ -28,13 +26,13 @@ class ReviewsController < ApplicationController
   	apt_record.save;
 
   	#render json to return the newly created review
-  	render json: {user_id: params[:user_id], apt_id: params[:apt_id], review: params[:review], overall_rating[new_average]}
+  	render json: {user_id: params[:user_id], apt_id: params[:apt_id], review: params[:review], overall_rating: new_average}
 
   end
 
   def update
     if(current_user)
-      #check the current user modifying the review is the user wrote the review
+      #check if the current user modifying the review is the user wrote the review
       review = Review.find(params[:review_id])
       user_id = review.user_id
       if(current_user.user_id != user_id)
@@ -47,7 +45,7 @@ class ReviewsController < ApplicationController
       #handle error: user is not logged in
       render json: {errCode: NOT_LOGGEDIN}
     end
-    
+
     if code!= 1
       #error handling, only FORBIDDEN will be returned in this case
       render json: {errCode: code}
@@ -62,6 +60,7 @@ class ReviewsController < ApplicationController
     new_average = Review.update_average_rating(num_of_reviews, average_review, old_overall_rating, new_overall_rating)
     apt_record.average_overall_rating = new_average;
     apt_record.save;     
+  end
 
   def find_by_apt
   	array_of_records = Review.find_by_apt(params[:apt_id])
@@ -70,7 +69,7 @@ class ReviewsController < ApplicationController
   	array_of_records.each_with_index{ |val, index|
   		review_id = val.review_id
   		user_id = val.user_id
-  		username = User.find(user_id).name 		#Todo: name is username or real name?
+  		username = User.find(user_id).username 		#Todo: name is username or real name?
   		review = val.review
   		overall_rating = val.overall_rating
   		json_object = {:review_id=>review_id, :username=>username, :review=>review, :overall_rating=>overall_rating}.to_json
