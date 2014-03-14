30865670978ea04fb9ed7bfe142179b03b952574
Samuel Son <sam.sik.son@gmail.com>
2014-03-13 19:18:59 -0700

    added update_average_rating and ad_average_rating to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,7 @@
 class Review < ActiveRecord::Base
-  ERROR = -1
-  BAD_USERNAME = -2
-  BAD_PASSWORD = -3
-  BAD_EMAIL = -4
-  BAD_CREDENTIALS = -5
+
+  SUCCESS = 1
   FORBIDDEN = 403
-  PAGE_NOT_FOUND = 404
 
   validates :user_id, presence: true
   validates :apt_id, presence: true
@@ -16,28 +12,40 @@ class Review < ActiveRecord::Base
     review = self.new(user: user_id, apartment: apt_id, overall_rating: overall_rating, review: review)
     if review.valid?
       review.save
+      return SUCCESS
     else
       return FORBIDDEN
     end
-      total = num_of_reviews * average_review + params[:overall_rating]
-      num_of_reviews += 1
-      new_average = total / num_of_reviews
-    end
+  end
 
-    def self.update(user_id, apt_id, overall_rating, review)
-      review = self.find_by_review(user: user_id, apartment: apt_id, overall_rating: overall_rating, review: review) #check find_by_review
-      if review.valid?
-        review.save
+    def self.update(review_id, overall_rating, review)
+      apt_reviews = self.find(review_id) #check find_by_review
+      if apt_reviews
+        apt_reviews.overall_rating = overall_rating
+        apt_reviews.review = review
+        apt_reviews.save
+        return SUCCESS
       else
         return FORBIDDEN
       end
-      total = num_of_reviews * average_review + params[:overall_rating]
-      num_of_reviews += 1
-      new_average = total / num_of_reviews
     end
 
     def self.find_by_apt(apt_id)
-      apt_reviews = Apartment.find(apt_id)
-      return reviews
+      apartment = Apartment.find(apt_id)
+      return apartment.Reviews
     end
+
+    def self.add_average_rating(num_of_reviews, average_review, overall_rating)
+      total = num_of_reviews * average_review + overall_rating
+      num_of_reviews += 1
+      new_average = total / num_of_reviews
+      return new_average
+    end
+
+  def self.update_average_rating(num_of_reviews, average_review, old_rating, new_rating)
+    total = num_of_reviews * average_review - old_rating + new_rating
+    new_average = total / num_of_reviews
+    return new_average
+  end
+
   end
