c6176adba0e9db080ea72a6bdbc7917da9ccff95
Lesley <chiang.lesley@gmail.com>
2014-03-14 05:26:28 -0700

    factory testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
-	factory :user do |f|
-		f.email "email@email.com"
-		f.username "user"
-		f.encrypted_password "pass"
+	factory :user do 
+		email "email@email.com"
+		username "user"
+		encrypted_password "pass"
 	end
 end
