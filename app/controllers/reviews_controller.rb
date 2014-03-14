1d3a7eef3acf94ede675ae5eaa4c33c9e590d38a
samsikson <sam.sik.son@gmail.com>
2014-03-13 22:24:11 -0700

    added attributes to review table and updated review model and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,14 @@
 class ReviewsController < ApplicationController
-	FORBIDDEN = 403
+  FORBIDDEN = 403
   SUCCESS = 1
   NOT_LOGGEDIN = -6
   UNAUTHORIZED_USER = -7
 
   def create
-  	#assuming add method in model will return correct error code or new_average
-  	if(current_user)	#check if the user is loggin in
-  		code = Review.add(params[:user_id],params[:apt_id], params[:overall_rating], params[:review]) 
-  	else
+    #assuming add method in model will return correct error code or new_average
+    if(current_user)	#check if the user is loggin in
+      code = Review.add(params[:user_id],params[:apt_id], params[:overall_rating], params[:review])
+    else
       render json: {errCode: NOT_LOGGEDIN}
     end
 
@@ -16,17 +16,17 @@ class ReviewsController < ApplicationController
       render json: {errCode: code}
     end
 
-  	#update new averge rating
-  	num_of_reviews = Review.count(:conditions => "apt_id == params[:apt_id]") 	#To do: make sure this is correct
+    #update new averge rating
+    num_of_reviews = Review.count(:conditions => "apt_id == params[:apt_id]") 	#To do: make sure this is correct
     apt_record = Apartment.find(params[:apt_id])
-  	average_review = apt_record.average_overall_rating
+    average_review = apt_record.average_overall_rating
     overall_rating = params[:overall_rating]
-  	new_average = Review.add_average_rating(num_of_reviews, average_review, overall_rating)
-  	apt_record.average_overall_rating = new_average;
-  	apt_record.save;
+    new_average = Review.add_average_rating(num_of_reviews, average_review, overall_rating)
+    apt_record.average_overall_rating = new_average;
+    apt_record.save;
 
-  	#render json to return the newly created review
-  	render json: {user_id: params[:user_id], apt_id: params[:apt_id], review: params[:review], overall_rating: new_average}
+    #render json to return the newly created review
+    render json: {user_id: params[:user_id], apt_id: params[:apt_id], review: params[:review], overall_rating: new_average}
 
   end
 
@@ -39,7 +39,7 @@ class ReviewsController < ApplicationController
         render json:{errCode: UNAUTHORIZED_USER}
       end
 
-      code = Review.update(params[:review_id], params[:overall_rating], params[:review]) 
+      code = review.update(params[:overall_rating], params[:review])
 
     else  #if not logged in
       #handle error: user is not logged in
@@ -49,7 +49,7 @@ class ReviewsController < ApplicationController
     if code!= 1
       #error handling, only FORBIDDEN will be returned in this case
       render json: {errCode: code}
-    end 
+    end
 
     #update new averge rating
     num_of_reviews = Review.count(:conditions => "apt_id == params[:apt_id]")   #To do: make sure this is correct
@@ -59,22 +59,22 @@ class ReviewsController < ApplicationController
     old_overall_rating = review.overall_rating
     new_average = Review.update_average_rating(num_of_reviews, average_review, old_overall_rating, new_overall_rating)
     apt_record.average_overall_rating = new_average;
-    apt_record.save;     
+    apt_record.save;
   end
 
   def find_by_apt
-  	array_of_records = Review.find_by_apt(params[:apt_id])
-  	array_length = array_of_records.array_length
-  	new_array = Array.new(array_length)
-  	array_of_records.each_with_index{ |val, index|
-  		review_id = val.review_id
-  		user_id = val.user_id
-  		username = User.find(user_id).username 		#Todo: name is username or real name?
-  		review = val.review
-  		overall_rating = val.overall_rating
-  		json_object = {:review_id=>review_id, :username=>username, :review=>review, :overall_rating=>overall_rating}.to_json
-  		new_array[index] = json_object
-  	}
-  	return new_array
+    array_of_records = Review.find_by_apt(params[:apt_id])
+    array_length = array_of_records.array_length
+    new_array = Array.new(array_length)
+    array_of_records.each_with_index{ |val, index|
+      review_id = val.review_id
+      user_id = val.user_id
+      username = User.find(user_id).username 		#Todo: name is username or real name?
+      review = val.review
+      overall_rating = val.overall_rating
+      json_object = {:review_id=>review_id, :username=>username, :review=>review, :overall_rating=>overall_rating}.to_json
+      new_array[index] = json_object
+    }
+    return new_array
   end
 end
--- db/migrate/20140311203453_create_reviews.rb
+++ db/migrate/20140311203453_create_reviews.rb
@@ -1,7 +1,12 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+      t.integer :user_id
+      t.integer :apt_id
+      t.date :created_date
+      t.text :review
+      t.integer :overall_rating
+      
       t.timestamps
     end
   end
