c5c941d534da80bb76b6319a1016b643d2a40198
Lesley <chiang.lesley@gmail.com>
2014-03-13 17:31:44 -0700

    update apartment model

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,6 +2,9 @@ class Apartment < ActiveRecord::Base
 	has_many :reviews
 	belongs_to :user
 
+	validates :title, :address, :description, :price, :bedrooms, :bathrooms, presence: true
+	validates :address, uniqueness: true
+
   	FORBIDDEN = 403
 
 	def self.add(user_id, title, address, desc, price, bedrooms, bathrooms)
@@ -10,7 +13,6 @@ class Apartment < ActiveRecord::Base
 			#ERROR
 		elsif title == nil || address == nill || desc == nill || price == nill || bedrooms == nill || bathrooms == nill
 			#ERROR
-		elsif #user not logged in
 		else
 			apt = self.new(title: title, address: address, description: desc, price: price, bedrooms: bedrooms, bathrooms:bathrooms)
 			#SUCCESS
@@ -22,14 +24,16 @@ class Apartment < ActiveRecord::Base
 			#ERROR
 		elsif title == nil || address == nill || desc == nill || price == nill || bedrooms == nill || bathrooms == nill
 			#ERROR
-		elsif #user not logged in
 		else
 			apt = self.new(title: title, address: address, description: desc, price: price, bedrooms: bedrooms, bathrooms:bathrooms)
 			#SUCCESS
 	end
 
-	def self.search(address)
-
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+		else
+			find(:all)
+		end
 	end
-
 end
