3ec72c3d9208154851cd00c4574488cd8cbea68d
samsikson <sam.sik.son@gmail.com>
2014-03-14 05:36:59 -0700

    unit test 4 saved reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,8 +4,10 @@ describe Review do
   it "find reviews by apartment_id" do
     apt1 = Apartment.add(1, "Beautiful Studio", "2402 Fulton St", "Sid loves this place! :)", 1000, 1, 1)
     review1 = apt1.reviews.add(1, 5, "This place is awesome!")
-    review2 = apt1.reviews.add(1, 5, "This place is aiteeee.")
+    review2 = apt1.reviews.add(1, 2, "This place is aiteeee.")
+    review3 = apt1.reviews.add(1, 5, "This place is grrreeatttt!")
+    review4 = apt1.reviews.add(1, 4, "This place is cool!")
     allReviews = Review.find_by_apt(apt1.id)
-    expect(allReviews.length).to eq(2)
+    expect(allReviews.length).to eq(4)
   end
 end
