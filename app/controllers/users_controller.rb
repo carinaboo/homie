6eb3b07db49ad71803202bea2b61f61ea8868d9e
Rico Ardisyah <rardisyah@gmail.cm>
2014-03-24 18:35:18 -0700

    Show apt list created by an user in his profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+
+	# This is the function to show a user page and list of apartments that the user create
 	def show
 		if(current_user)
 			@user = User.find(params[:id])
+			# @orders = Order.where(customer_id: @customer.id)
+			@apartments = Apartment.where(user_id: @user.id)
 		else
 			render json: {errCode: NOT_LOGGEDIN}
 		end
