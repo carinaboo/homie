4a00cc98d8e6d89f602d8b2e91923fc7e727ddf7
Lesley <chiang.lesley@gmail.com>
2014-04-01 21:04:35 -0700

    factories update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ReviewsController < ApplicationController
   FORBIDDEN = 403
   PAGE_NOT_FOUND = 404
@@ -8,101 +7,7 @@ class ReviewsController < ApplicationController
 
   def create
     # can only create when in an apartment page!
-    if(current_user)	# check if the user is loggin in
-
-      #check if the current user already has a review for this apartment
-      apartment = Apartment.find(params[:review][:apartment_id])  
-      existance = false
-      apartment.reviews.each do |r|
-        if r.apartment_id == params[:review][:apartment_id]
-          existance = true
-        end
-      end
-
-      if existance == false
-        render json: {errCode: FORBIDDEN}
-      end
-
-      # update new averge rating
-      num_of_reviews = apartment.reviews.count
-      average_review = apartment.average_overall_rating || 0
-      overall_rating = params[:review][:overall_rating].to_i
-      new_average = Review.add_average_rating(num_of_reviews, average_review, overall_rating)
-      apartment.average_overall_rating = new_average;
-      apartment.save;
-      # add review to apartment
-      review = apartment.reviews.add(current_user.id, params[:review][:overall_rating], params[:review][:review])
-      redirect_to apartment_path id: apartment.id
-    else
-      render json: {errCode: NOT_LOGGEDIN}
-    end
-  end
-
-  #Page for editing review
-  def edit
-    @review = Review.find(params[:id])
-    if !@review
-      render json: {errCode: PAGE_NOT_FOUND}
-    elsif !current_user
-      render json: {errCode: FORBIDDEN}
-    end
-  end
-
-  def update
-    if(current_user)
-      review = Review.find(params[:id])
-      apartment = Apartment.find(review.apartment_id)
-      # check if the current user modifying the review is the user wrote the review
-      user_id = review.user_id
-      if(current_user.id != user_id)
-        render json:{errCode: UNAUTHORIZED_USER}
-      else
-        # update new averge rating
-        num_of_reviews = apartment.reviews.count
-        average_review = apartment.average_overall_rating || 0
-        new_overall_rating = params[:review][:overall_rating].to_i
-        old_overall_rating = review.overall_rating
-        new_average = Review.update_average_rating(num_of_reviews, average_review, old_overall_rating, new_overall_rating)
-        apartment.average_overall_rating = new_average;
-        apartment.save;
-        # add review to apartment
-        review.update(params[:review][:overall_rating], params[:review][:review])
-        redirect_to apartment_path id: apartment.id
-      end
-    else
-      # user is not logged in
-      render json: {errCode: NOT_LOGGEDIN}
-    end
-  end
-
-  def find_by_apt
-    reviews = Review.find_by_apt(params[:apt_id])
-    render json: reviews.to_json
-    # array_length = array_of_records.array_length
-    # new_array = Array.new(array_length)
-    # array_of_records.each_with_index{ |val, index|
-    #   review_id = val.review_id
-    #   user_id = val.user_id
-    #   username = User.find(user_id).username 		#Todo: name is username or real name?
-    #   review = val.review
-    #   overall_rating = val.overall_rating
-    #   json_object = {:review_id=>review_id, :username=>username, :review=>review, :overall_rating=>overall_rating}.to_json
-    #   new_array[index] = json_object
-    # }
-    # return new_array
-  end
-end
-=======
-class ReviewsController < ApplicationController
-  FORBIDDEN = 403
-  PAGE_NOT_FOUND = 404
-  SUCCESS = 1
-  NOT_LOGGEDIN = -6
-  UNAUTHORIZED_USER = -7
-
-  def create
-    # can only create when in an apartment page!
-    if(current_user)	# check if the user is loggin in
+    if(current_user)  # check if the user is loggin in
       apartment = Apartment.find(params[:review][:apartment_id])   
       # update new averge rating
       num_of_reviews = apartment.reviews.count
@@ -164,7 +69,7 @@ class ReviewsController < ApplicationController
     # array_of_records.each_with_index{ |val, index|
     #   review_id = val.review_id
     #   user_id = val.user_id
-    #   username = User.find(user_id).username 		#Todo: name is username or real name?
+    #   username = User.find(user_id).username    #Todo: name is username or real name?
     #   review = val.review
     #   overall_rating = val.overall_rating
     #   json_object = {:review_id=>review_id, :username=>username, :review=>review, :overall_rating=>overall_rating}.to_json
@@ -172,27 +77,4 @@ class ReviewsController < ApplicationController
     # }
     # return new_array
   end
-
-  def delete
-    review = Review.find(params[:id])
-    @apartment = Apartment.find(review.apartment_id)
-    # check if the current user modifying the review is the user wrote the review
-    user_id = review.user_id
-    if !@apartment
-      flash[:error] = "Error: Apartment page not found\n"
-      redirect_to root_path
-    elsif current_user.id != user_id
-      #user is not review creator
-      flash[:error] = "Error: only the owner is allowed to delete apartment\n"
-      redirect_to @apartment
-    elsif !current_user
-      # user is not logged in
-      flash[:error] = "Error: user must be signed in to delete apartment\n"
-      redirect_to @apartment
-    else
-      Review.delete(user_id, review.apartment_id)
-      redirect_to @apartment
-    end
-  end
 end
->>>>>>> FETCH_HEAD
--- spec/factories/apartments.rb
+++ spec/factories/apartments.rb
@@ -4,7 +4,11 @@
 #
 #  id                     :integer          not null, primary key
 #  title                  :string(255)
-#  address                :string(255)
+#  street_address         :string(255)
+#  apartment_number       :string(255)
+#  city                   :string(255)
+#  state                  :string(255)
+#  zip                    :integer
 #  description            :text
 #  price                  :float
 #  bathrooms              :float
@@ -13,12 +17,15 @@
 #  created_at             :datetime
 #  updated_at             :datetime
 #  user_id                :integer
-#
 
 FactoryGirl.define do 
 	factory :apartment do |f|
 		f.title "Title"
-		f.address "2137 Parker St."
+		f.street_address "2137 Parker St"
+		f.apartment_number "10"
+		f.city "Berkeley"
+		f.state "California"
+		f.zip "94704"
 		f.description "home"
 		f.price 450.50
 		f.bedrooms 5
