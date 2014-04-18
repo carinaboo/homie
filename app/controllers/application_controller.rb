2fe657c9798769df245229193486c483247f8049
wayu93 <yuhuaw@berkeley.edu>
2014-04-17 19:38:10 -0700

    can edit username and add pictures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :photo) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :photo, :current_password) }
   end
 
   protect_from_forgery with: :exception
